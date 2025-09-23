3803f12421a42e7694d195553d5f29bce2c258f4
static09 <m.static09.m@gmail.com>
2025-09-22 23:06:47 -0700

    Added tick rate to game

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ def main():
     print("Screen height:", SCREEN_HEIGHT)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock()
+    dt = 0
     while pygame.get_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.flip()
+    pygame.time.Clock.tick(60)
+    dt = pygame.time.Clock.tick()/1000
 
 
 if __name__ == "__main__":
