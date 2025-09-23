bb32104a8c5846e3be107f31671b41ee9d861679
static09 <m.static09.m@gmail.com>
2025-09-22 22:55:40 -0700

    First commit with some basic starting lines of code for Astroids game

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while pygame.get_init():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
