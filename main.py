f2a977c25604889b133561c97bcb492273fbfc50
static09 <m.static09.m@gmail.com>
2025-09-23 21:22:00 -0700

    Added a player character to be drawn on the screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
-from CircleShape import *
+from circleshape import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -9,15 +10,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     while pygame.get_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+        player.draw(screen)
         pygame.display.flip()
     pygame.time.Clock.tick(60)
-    dt = pygame.time.Clock.tick()/1000
+    dt = pygame.time.Clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
