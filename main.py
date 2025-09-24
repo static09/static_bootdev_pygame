d0b4983c1a5c0bb78f7784fb900eab48c69b31c9
static09 <m.static09.m@gmail.com>
2025-09-23 21:55:02 -0700

    Added rotation and fixed main.py to properly draw things on the screen. Previous code was not allowing the player to rotate.

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import pygame
 from constants import *
-from circleshape import *
 from player import Player
 
+
 def main():
-    print("Starting Asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.time.Clock()
+    clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    while pygame.get_init():
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+        player.update(dt)
+
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-    pygame.time.Clock.tick(60)
-    dt = pygame.time.Clock.tick(60) / 1000
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
+
--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import pygame
 from circleshape import CircleShape
 from constants import *
 
+
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
@@ -10,7 +11,6 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
-    # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -18,3 +18,15 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
